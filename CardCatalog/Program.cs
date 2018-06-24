eb2c1980e37489235c2b9c5c6536d966926be44e
asafdar <asafdar1138@gmail.com>
2018-06-24 18:03:21 -0500

    Add Program.Main
    Minor changes to AddBook() and ListBooks()

--- CardCatalog/Program.cs
+++ CardCatalog/Program.cs
@@ -6,6 +6,40 @@ using System.Threading.Tasks;
 
 namespace CardCatalog
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a file name: ");
+            string fileName = Console.ReadLine();
+            List<Book> books = new List<Book>();
+
+            int input = 0;
+
+            while (input != 3)
+            {
+                Console.WriteLine("\n1. Add a Book\n2. List all books\n3. Save and exit");
+                input = int.Parse(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                        CardCatalog.AddBook(books);
+                        continue;
+                    case 2:
+                        CardCatalog.ListBooks(books);
+                        continue;
+                    case 3:
+                        CardCatalog.Save(books);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+    
     [Serializable]
     public class CardCatalog
     {
@@ -17,19 +51,36 @@ namespace CardCatalog
            _filename = fileName;
         }
 
-        public void ListBooks()
+        public static void ListBooks(List<Book> books)
         {
-            foreach (Book book in books){
-                Console.WriteLine("Author: " + book.author + ", Title: " + book.title);
+            foreach (var book in books)
+            {
+                Console.WriteLine("Author: " + book.Author + "\t\tTitle: " + book.Title + "\t\tISBN: " + book.ISBN);
+                Console.Clear();
             }
+
         }
 
-        public void AddBook(string author, string title)
+        public static List<Book> AddBook(List<Book> books)
         {
-            Book newBook = new Book(author, title);
-            books.Add(newBook);
+            Console.Write("\nEnter the title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Enter the author: ");
+            string author = Console.ReadLine();
+
+            Console.Write("Enter the ISBN: ");
+            string isbn = Console.ReadLine();
+
+            books.Add(new Book(title, author, isbn));
+            Console.Clear();
+
+            return books;
         }
+        
+        public static void Save(List<Book> books)
+        {
 
-
+        }
     }
 }
